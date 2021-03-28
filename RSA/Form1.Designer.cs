f86c88530a35b538c13592e9366fc1062c45a444
KatinasPukis <65959494+KatinasPukis@users.noreply.github.com>
2021-03-28 19:39:10 +0300

    Added public key
    p, q, n, F(n), public key

--- RSA/Form1.Designer.cs
+++ RSA/Form1.Designer.cs
@@ -121,6 +121,7 @@
             this.button1.TabIndex = 10;
             this.button1.Text = "Encrypt/Decrypt";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- RSA/Form1.cs
+++ RSA/Form1.cs
@@ -15,7 +15,69 @@ namespace RSA
         public Form1()
         {
             InitializeComponent();
-            Console.WriteLine("Inittial commit");
+            
+
         }
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+        private void CheckNumbers(int number)
+        {
+            
+            if(IsPrime(number)== false)
+            {
+                MessageBox.Show(number + " " + "is not a prime number");
+            }
+        }
+        private int CommonDivisor(int a, int b)
+        {
+            int remainder;
+            while(b!=0)
+            {
+                remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+            return a;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int exponent = 0;
+            int p = int.Parse(TextboxP.Text);
+            int q = int.Parse(TextBoxQ.Text);
+            CheckNumbers(p);
+            CheckNumbers(q);
+            int n = p * q;
+            Console.WriteLine("n="+ " "+n);
+            int Fn = (p - 1) * (q - 1);
+            Console.WriteLine("F(n)="+" "+Fn);
+            for(int i =2; i<Fn; i++)
+            {
+                if(CommonDivisor(i,Fn)==1)
+                {
+                    exponent = i;
+                    if(exponent>1)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Public key="+" "+ "e="+" "+exponent+" "+ "n="+" "+n);
+
+        }
+
     }
 }
